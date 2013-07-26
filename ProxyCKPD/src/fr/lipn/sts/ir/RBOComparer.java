c7351b30a2611bfbb8b1cb4038bf8a81a2153f64
dbuscaldi <davide.buscaldi@gmail.com>
2013-07-26 16:21:14 +0200

    Added RBO Comparer based on Webber, Moffat, Zobel: A similarity measure for Indefinite Rankings

--- ProxyCKPD/src/fr/lipn/sts/ir/RBOComparer.java
+++ ProxyCKPD/src/fr/lipn/sts/ir/RBOComparer.java
@@ -1,7 +1,6 @@
 package fr.lipn.sts.ir;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -19,11 +18,16 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 import fr.lipn.sts.SemanticComparer;
-
-public class IRComparer {
+/**
+ * This class implements the RBO measure by Webber, Moffat, Zobel (2010) "A similarity measure for indefinite rankings"
+ * @author buscaldi
+ *
+ */
+public class RBOComparer {
 	private static String index = "/tempo/corpora/AQUAINT_indexed";
 	
-	private final static int K=70;
+	private final static double p=0.95; //RBO parameter
+	private final static int MAXHITS=100;
 	
 	public static double compare(String req1, String req2){
 		double ret = 0d;
@@ -42,65 +46,70 @@ public class IRComparer {
 			Query query1 = parser.parse(req1.trim());
 			Query query2 = parser.parse(req2.trim());
 			
-			if(SemanticComparer.VERBOSE) System.err.println("searching queries:\nQ1: "+query1.toString()+"\nQ2: "+query2.toString());
+			if(SemanticComparer.VERBOSE) System.err.println("searching queries (RBO):\nQ1: "+query1.toString()+"\nQ2: "+query2.toString());
 			
-			TopDocs results1 = searcher.search(query1, 100);
+			TopDocs results1 = searcher.search(query1, MAXHITS);
 			ScoreDoc[] hits1 = results1.scoreDocs;
 			
-			TopDocs results2 = searcher.search(query2, 100);
+			TopDocs results2 = searcher.search(query2, MAXHITS);
 			ScoreDoc[] hits2 = results2.scoreDocs;
 			
 		    
 		    int n_1 = results1.totalHits;
 		    int n_2 = results2.totalHits;
 		    
-		    HashMap<String, Double> m1 = new HashMap<String, Double>();
-		    HashMap<String, Double> m2 = new HashMap<String, Double>();
+		    HashMap<Integer, HashSet<String>> q1sets = new HashMap<Integer, HashSet<String>>();
+		    HashMap<Integer, HashSet<String>> q2sets = new HashMap<Integer, HashSet<String>>();
 		    
 		    //System.err.println(numTotalHits + " total matching documents");
 		    if(n_1 > 0 && n_2 > 0) {
-		    	for (int i = 0; i < Math.min(K, n_1); i++) {
+		    	for (int i = 0; i < Math.min(MAXHITS, n_1); i++) {
 		    		Document doc = searcher.doc(hits1[i].doc);
 			        String id = doc.get("id");
-			        Double score = new Double(hits1[i].score);
-			        m1.put(id, score);
-			        //if(SemanticComparer.VERBOSE) System.err.println("req1 doc: "+id+" : "+score);
+			        Integer k1= new Integer(i+1);
+			        HashSet<String> kd= q1sets.get(new Integer(i));
+			        HashSet<String> newKD = new HashSet<String>();
+			        if(kd!=null){
+			        	newKD.addAll(kd);
+			        }
+			        newKD.add(id);
+		        	q1sets.put(k1, newKD);
+			        
 		    	}
 		    	
-		    	for (int i = 0; i < Math.min(K, n_2); i++) {
+		    	for (int i = 0; i < Math.min(MAXHITS, n_2); i++) {
 		    		Document doc = searcher.doc(hits2[i].doc);
 			        String id = doc.get("id");
-			        Double score = new Double(hits2[i].score);
-			        m2.put(id, score);
-			        //if(SemanticComparer.VERBOSE) System.err.println("req2 doc: "+id+" : "+score);
+			        Integer k2= new Integer(i+1);
+			        HashSet<String> kd= q2sets.get(new Integer(i));
+			        HashSet<String> newKD = new HashSet<String>();
+			        if(kd!=null){
+			        	newKD.addAll(kd);
+			        }
+			        newKD.add(id);
+		        	q2sets.put(k2, newKD);
 		    	}
 		    	
 		    	HashSet<String> uniqueIDs = new HashSet<String>();
-		    	uniqueIDs.addAll(m1.keySet());
-		    	uniqueIDs.retainAll(m2.keySet()); //intersection
-		    	//uniqueIDs.addAll(m2.keySet()); //union
-		    	
-		    	if(uniqueIDs.size() == 0) return 0d; //no common docs
-		    	
-		    	double sum=0d;
-		    	for(String key : uniqueIDs) {
-		    		
-		    		Double v1 = m1.get(key);
-		    		double s1;
-		    		if(v1==null) s1=0d;
-		    		else s1=v1.doubleValue();
-		    		
-		    		Double v2 = m2.get(key);
-		    		double s2;
-		    		if(v2==null) s2=0d;
-		    		else s2=v2.doubleValue();
-		    		
-		    		if(SemanticComparer.VERBOSE) System.err.println("common doc: "+key+" s1: "+s1+" s2: "+s2);
-		    		
-		    		sum+=(Math.sqrt(Math.pow((s1-s2), 2)))/Math.max(s1, s2);
+		    	for(int i = 1; i< MAXHITS; i++) {
+		    		Integer d=new Integer(i);
+		    		uniqueIDs.clear();
+		    		HashSet<String> m1 = q1sets.get(d);
+		    		HashSet<String> m2 = q2sets.get(d);
+		    		double Ad=0d;
+		    		if(m1 != null && m2 != null){
+		    			uniqueIDs.addAll(m1);
+				    	uniqueIDs.retainAll(m2); //intersection
+				    	Ad=(double)uniqueIDs.size()/d.doubleValue();
+				    	//System.err.println("A"+d+" overlap: "+Ad);
+		    		}
+		    		double rbod=Math.pow(p, d.doubleValue()-1)*Ad;
+		    		//System.err.println("RBO at "+d+": "+rbod);
+		    		ret+=rbod;
 		    	}
 		    	
-		    	ret=1-(sum/(double)uniqueIDs.size());
+		    	ret=ret*(1-p);
+		    	
 		    }
 			
 		    reader.close();
@@ -110,6 +119,4 @@ public class IRComparer {
 		
 		return ret;
 	}
-	
-
 }
