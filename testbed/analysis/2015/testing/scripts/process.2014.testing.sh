ef543c26435d2b6cbf7a8ae4c080003a96ad5656
Jorge García Flores <jgflores@lipn.fr>
2014-12-17 16:19:38 -0600

    2014.testing data added

---
+++ testbed/analysis/2015/testing/scripts/process.2014.testing.sh
@@ -0,0 +1,6 @@
+STS.2014.input.tweet-news.txt
+STS.2014.test.input.deft-forum.txt
+STS.2014.test.input.deft-news.txt
+STS.2014.test.input.headlines.txt
+STS.2014.test.input.images.txt
+STS.2014.test.input.OnWN.txt
