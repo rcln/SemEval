4ef81dd9032f8dc90d83ac562497e43347099b87
rcln <jgflores@lipn.fr>
2014-12-16 20:05:54 -0600

    2013 script

--- testbed/analysis/2015/training/scripts/process.train.and.test.2013.sh
+++ testbed/analysis/2015/training/scripts/process.train.and.test.2013.sh
@@ -1,8 +1,6 @@
-STS.2013.test.gs.FNWN.txt
-STS.2013.test.gs.headlines.txt
-STS.2013.test.gs.OnWN.txt
-STS.2013.test.gs.SMT.txt
-STS.2013.test.input.FNWN.txt
-STS.2013.test.input.headlines.txt
-STS.2013.test.input.OnWN.txt
-STS.2013.test.input.SMT.txt
+#!/bin/sh
+export SEMEVAL_HOME=/local_home/lipn/SemEval/
+./run.semantic.comparer.with.train.sh $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.input.FNWN.txt $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.gs.FNWN.txt
+./run.semantic.comparer.with.train.sh $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.input.headlines.txt $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.gs.headlines.txt
+./run.semantic.comparer.with.train.sh $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.input.OnWN.txt $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.gs.OnWN.txt
+./run.semantic.comparer.with.train.sh $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.input.SMT.txt $SEMEVAL_HOME/testbed//data/2014/train/STS.2013.test.gs.SMT.txt
