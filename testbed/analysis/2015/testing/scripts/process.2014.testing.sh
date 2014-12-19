aeb2ce6d6a02133b320420f7c88e2d364b5f3170
Jorge García Flores <jgflores@lipn.fr>
2014-12-19 17:04:23 -0600

    process english testing

--- testbed/analysis/2015/testing/scripts/process.2014.testing.sh
+++ testbed/analysis/2015/testing/scripts/process.2014.testing.sh
@@ -1,6 +1,8 @@
-STS.2014.input.tweet-news.txt
-STS.2014.test.input.deft-forum.txt
-STS.2014.test.input.deft-news.txt
-STS.2014.test.input.headlines.txt
-STS.2014.test.input.images.txt
-STS.2014.test.input.OnWN.txt
+#!/bin/sh
+export SEMEVAL_HOME=/home/jgflores/experimentos/SemEval/sopa.2015/SemEval/
+./run.semantic.comparer.sh $SEMEVAL_HOME/testbed/data/2014/test/STS.2014.input.tweet-news.txt> $SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.results/STS.2014.input.tweet-news.dat 2>$SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.logs/STS.2014.input.tweet-news.log
+./run.semantic.comparer.sh $SEMEVAL_HOME/testbed/data/2014/test/STS.2014.test.input.deft-forum.txt> $SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.results/STS.2014.test.input.deft-forum.dat 2>$SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.logs/STS.2014.test.input.deft-forum.log
+./run.semantic.comparer.sh $SEMEVAL_HOME/testbed/data/2014/test/STS.2014.test.input.deft-news.txt> $SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.results/STS.2014.test.input.deft-news.dat 2>$SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.logs/STS.2014.test.input.deft-news.log
+./run.semantic.comparer.sh $SEMEVAL_HOME/testbed/data/2014/test/STS.2014.test.input.headlines.txt> $SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.results/STS.2014.test.input.headlines.dat 2>$SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.logs/STS.2014.test.input.headlines.log
+./run.semantic.comparer.sh $SEMEVAL_HOME/testbed/data/2014/test/STS.2014.test.input.images.txt> $SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.results/STS.2014.test.input.images.dat 2>$SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.logs/STS.2014.test.input.images.log
+./run.semantic.comparer.sh $SEMEVAL_HOME/testbed/data/2014/test/STS.2014.test.input.OnWN.txt> $SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.results/STS.2014.test.input.OnWN.dat 2>$SEMEVAL_HOME/testbed/analysis/2015/testing/2014.test.logs/STS.2014.test.input.OnWN.log
