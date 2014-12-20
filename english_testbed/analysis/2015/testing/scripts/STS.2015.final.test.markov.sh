9c9bace0eddc3d526c82daa4d31070d46c6c06c6
rcln <jgflores@lipn.fr>
2014-12-20 10:41:57 -0600

    markov scripts for english

---
+++ english_testbed/analysis/2015/testing/scripts/STS.2015.final.test.markov.sh
@@ -0,0 +1,2 @@
+./run.semantic.comparer.sh $ENGLISH_TESTBED/data/2015/test/STS.input.answers-students.txt > $ENGLISH_TESTBED/analysis/2015/testing/results/STS.input.answers-students.dat 2>$ENGLISH_TESTBED/analysis/2015/testing/logs/STS.input.answers-students.log
+
