019e0a4f004ee962933f66d212aa72ee1a891d6a
Garcia flores Jorge <garciaflores@tal-semeval.tal>
2016-02-04 19:17:17 +0100

    first results for crosslingual

---
+++ crosslingual_testbed/analysis/2016/testing/scripts/STS.2016.monolingual.comparer.sh
@@ -0,0 +1,2 @@
+./run.semantic.comparer.sh $CROSSLINGUAL_TESTBED/data/2016/test/STS.input.crosslingual-news.en.txt > $CROSSLINGUAL_TESTBED/analysis/2016/testing/results/STS.input.crosslingual-news.en.txt.dat 2>$CROSSLINGUAL_TESTBED/analysis/2016/testing/logs/STS.input.crosslingual-news.en.txt.log 
+./run.semantic.comparer.sh $CROSSLINGUAL_TESTBED/data/2016/test/STS2016.INPUT.crosslingual.part2.en.txt > $CROSSLINGUAL_TESTBED/analysis/2016/testing/results/STS2016.INPUT.crosslingual.part2.en.txt.dat 2>$CROSSLINGUAL_TESTBED/analysis/2016/testing/logs/STS2016.INPUT.crosslingual.part2.en.txt.log
