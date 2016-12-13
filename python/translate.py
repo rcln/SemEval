22907d5e67099d1e704ab9602870b1d6e43edf93
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2016-12-13 13:50:03 -0600

    Translations for spanish and arabic

--- python/translate.py
+++ python/translate.py
@@ -8,7 +8,9 @@ import codecs
 from utils import * 
 from collections import Counter
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import keys
+import time
 
 verbose = lambda *a: None 
 
@@ -43,16 +45,27 @@ def get_language(input_text):
  
 
 
-def translate(to_translate,outfile):
-    print [x[0] for x in to_translate]
-    translations=service.translations().list(
-        source=opts.source,
-        target=opts.target,
-        q=[x[0] for x in to_translate]
-                          ).execute()
-    for i,translation in enumerate(translations[u'translations']):
-        outfile.write(u"{0}\t{1}\n".format(translation[u'translatedText'],to_translate[i][1]))
-
+def translate(to_translate,outfile,first=False):
+    if first:
+        translations=service.translations().list(
+            source=opts.source,
+            target=opts.target,
+            q=[x[0] for x in to_translate]
+                              ).execute()
+        for i,translation in enumerate(translations[u'translations']):
+            outfile.write(u"{0}\t{1}\n".format(translation[u'translatedText'],to_translate[i][1]))
+    else:
+        sntcs=[]
+        for pair in to_translate:
+            sntcs.append(pair[0])
+            sntcs.append(pair[1])
+        translations=service.translations().list(
+                source=opts.source,
+                target=opts.target,
+                q=sntcs).execute()
+        translations=translations[u'translations']
+        for i in range(len(translations)/2):
+            outfile.write(u"{0}\t{1}\n".format(translations[2*i]['translatedText'],translations[2*i+1]['translatedText']))
 
 if __name__ == "__main__":
     #Las opciones de línea de comando
@@ -64,16 +77,18 @@ if __name__ == "__main__":
     p.add_argument("--source",default='es',
                 action="store", dest="source",type=str,
                 help="Source language")
+    p.add_argument("--crosslingual",default=False,
+                action="store_true", dest="crosslingual",
+                help="Croslingual corpus (only translate on)")
     p.add_argument("--target",default='en',
                 action="store", dest="target",type=str,
                 help="Target language")
-    p.add_argument("--filter",default='.',
-                action="store", dest="filter_test",type=str,
-                help="Regular expression to filter the test")
+    p.add_argument("--fortmat",default=None,
+                action="store", dest="format",type=str,
+                help="Format (None for most, 2017 for new crosslingual)")
     p.add_argument("-v", "--verbose",
                 action="store_true", dest="verbose",
                 help="Verbose mode [Off]")
- 
     opts = p.parse_args()
 
     if opts.verbose:
@@ -86,27 +101,33 @@ if __name__ == "__main__":
 
     train_data=[]
     verbose('Loading phrases in directory', opts.DIR)
-    data=load_all_phrases(opts.DIR,format='2017')
+    data=load_all_phrases(opts.DIR,format=opts.format)
     verbose('Total train phrases',sum([len(d) for n,d in train_data]))
     for filename,phrases in data:
         verbose('Procesando', filename)
         to_translate=[]
         total_chars=0
+        if os.path.exists(os.path.join(opts.OUTPUT,filename[:-4]+".translation.txt")):
+            continue
         with codecs.open(os.path.join(opts.OUTPUT,filename[:-4]+".translation.txt"),"w",encoding='utf-8') as outfile:
             for phrase in phrases:
-                if total_chars+len(phrase[0])>3000:
-                    translate(to_translate,outfile)
-                    to_translate=[]
-                    total_chars=0
-                l1=get_language(phrase[0])
-                l2=get_language(phrase[1])
-                if l1=="english":
-                    to_translate.append((phrase[1],phrase[0]))
+                if opts.crosslingual:
+                    if total_chars+len(phrase[0])>3000:
+                        translate(to_translate,outfile,first=True)
+                        to_translate=[]
+                        total_chars=0
+                    l1=get_language(phrase[0])
+                    l2=get_language(phrase[1])
+                    if l1=="english":
+                          to_translate.append((phrase[1],phrase[0]))
+                    else:
+                        to_translate.append(phrase)
+                    total_chars+=len(phrase[0])
                 else:
+                    if total_chars+len(phrase[0])+len(phrase[1])>3000:
+                        translate(to_translate,outfile)
+                        to_translate=[]
+                        total_chars=0
                     to_translate.append(phrase)
-                total_chars+=len(phrase[0])
-            translate(to_translate,outfile)
-
-
-
-
+                    total_chars+=len(phrase[0])+len(phrase[1])
+            translate(to_translate,outfile,first=opts.crosslingual)
